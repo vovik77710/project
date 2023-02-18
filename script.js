39d40097515c5dec8451743ccd4ce1f76b9a594c
Vladimir Gutsaluk <lv81@bk.ru>
2023-02-18 19:54:46 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,135 @@
+"use strict";
+
+// const vihicleBodyWidth = 5000,  vihicleBodyLength = 4000;
+
+// console.log("Ширина кузова авто: " + vihicleBodyWidth + ', длина: ' + vihicleBodyLength)
+
+// ['dsfdsfsd', 'dsfsdfsd', 'sfdfsdfsd'].map(a =>)
+
+//Request
+
+//snake_case
+
+//UPPER_SNAKE-CASE
+
+//Kebab-case
+
+//PascalCase
+
+// const COLOR_RED = '#F00';
+
+// let number = 4.6;
+// console.log(4/0)
+
+// const person = "5";
+
+// const bool = false
+
+// console.log(something)
+
+// let und
+// console.log(und)
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false,
+// };
+
+// console.log(obj['name'])
+
+// const arr = ['plum.png', 'orange', 6, 'apple.bmp',  {}, [] ];
+// console.log(arr[0])
+
+// const arr = [1, 2, 3];
+
+// console.log(arr[1]);
+
+// const obj = { a: 1, b: 2 };
+
+// const obj = {
+//   Anna: 500,
+//   Alice: 800,
+// };
+
+// console.log(obj);
+
+// const arr = ["a", "b", "c"];
+
+// arr[10] = '3456'
+// console.log(arr);
+
+// const arrObj = {
+//   a: 'a',
+//   1: 'b',
+//   2: 'c',
+//   abc: {
+//     df: [{}, {}],
+//     def: {
+
+//     }
+//   }
+// };
+
+// const b = 'b'
+
+// arrObj['b'] = '1234';
+
+// console.log(arr[1]);
+// console.log(arrObj.b);
+
+// const storeName = 'Daraboz';
+// console.log(storeName);
+
+// alert('hello')
+
+// const result = confirm('Hello?')
+// console.log(result);
+
+// const answer = prompt('are you 18 years old?', '18')
+// console.log(answer + 5);
+
+// const anwers = [];
+// anwers[0] = prompt("Как ваше имя", "");
+// anwers[1] = prompt("Как ваше фамилия", "");
+// anwers[2] = prompt("Как ваше отчество", "");
+
+// console.log(typeof (anwers));
+
+
+
+// const category = "toys";
+
+// console.log(`html://someurl.com/${category}`);
+
+// const user = "Vova";
+// alert(`How are you name? ${user}`);
+
+
+
+//.....................
+
+
+// console.log('arr' + " - object");
+// console.log(4 + +"5");
+
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+
+const isCheked = false
+    isClose = false
+
+    console.log(isCheked || !isClose);
